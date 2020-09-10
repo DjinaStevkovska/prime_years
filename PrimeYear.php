dc17d4fc19ac2ba76b1033bd4c01ffdfe44d1a9f
DjinaStevkovska <stevkovskadjina@yahoo.com>
2020-09-10 15:47:21 +0200

    database connection

--- PrimeYear.php
+++ PrimeYear.php
@@ -26,12 +26,41 @@ if (isset($_POST['print']) && !empty($_POST['year']))
         'month' => 12,
         'date' => 25
     );
+    
 
     foreach ($primeyears as $year) {
-        $calc =  $christmas['date'] .'-'. $christmas['month'] .'-'. $year;
-        echo "<br>";
-        echo date("l - Y-m-d", strtotime($calc))."<br>";  
+        $dateTime =  $christmas['date'] .'-'. $christmas['month'] .'-'. $year;
+        // echo "<br>";
+        // echo date("l - Y-m-d", strtotime($dateTime))."<br>";  
+        $onlyName = date("l", strtotime($dateTime));
+        $onlyYear = date("Y", strtotime($dateTime));
+
+
+        $conn = mysqli_connect("localhost", "homestead", "secret", "laravel_db");
+        $sql = "SELECT * FROM prime_years WHERE year='$onlyYear'";
+        $query = mysqli_query($conn, $sql);
+        $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+        // var_dump($result);
+        mysqli_close($conn);
+        $conn = mysqli_connect("localhost", "homestead", "secret", "laravel_db");
+
+
+        $fromdb = (!empty($result)) ? ($result[0]['year']) : '';
+
+            if (!$fromdb == $onlyYear) {
+                $insertsql = "INSERT INTO prime_years (year, day) 
+                VALUES($onlyYear, '$onlyName')";
+                $query = mysqli_query($conn, $insertsql);
+
+            } 
+ 
+        mysqli_close($conn);
+        // header("Locaton: http://app.test/steets/index.php");
+        
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+            // echo 'Connected successfully';
     }
 }
-
 ?>
